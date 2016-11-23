eb6c2171516e1871f04bc9ee94386761822aa5c0
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2016-11-23 14:01:47 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var form = document.querySelector('form');
+
+var validateForm = function(evt) {
+    evt.preventDefault();
+    // Select elements which have required attribute
+    // If the value of these inputs is empty
+    // Prevent submission
+    // Change the border color of invalid input to red
+    // end if
+}
+
+form.addEventListener('submit', validateForm);
